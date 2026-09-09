253b88d78056c699509e74b6e6d25c7b632f18a5
tabassumhadiya <tabassumhadiya32@gmail.com>
2026-09-09 10:01:35 +0530

    Add program to compare two numbers

---
+++ prgrm7.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print(a, "is greater")
+elif b > a:
+    print(b, "is greater")
+else:
+    print("Both numbers are equal")
